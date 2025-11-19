f80ca0a7b6efbcda3db75d59f042fafbfac45d6b
Ppantaleo <patriciopantaleo@hotmail.com>
2025-11-19 09:23:53 -0300

    nav

--- components/header.tsx
+++ components/header.tsx
@@ -44,7 +44,7 @@ export default function Header() {
 
           {/* Desktop Navigation */}
           <nav className="hidden md:flex items-center space-x-8">
-            <NavLink href="#about" label="ABOUT" />
+            <NavLink href="https://patricio.pantaleo.ar/#about" label="ABOUT" />
             <NavLink href="https://patricio.pantaleo.ar/blog" label="BLOG" />
             <NavLink href="/recursos" label="RESOURCES" />
           {/*  <NavLink href="#skills" label="SKILLS" />
